aa36c984c3b26d97f34f5120da4fa5df83eab514
Reuven-Jon Clements Kadalie <ST10271460@vcconnect.edu.za>
2024-09-09 13:21:07 +0200

    Added a Claim class to handle all user claims.

---
+++ CMCS.PROG6212.ST10271460/Models/Claim.cs
@@ -0,0 +1,26 @@
+﻿namespace CMCS.PROG6212.ST10271460.Models
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; } // Primary Key
+        public int ContractorID { get; set; } // Foreign Key to User
+
+        public DateTime ClaimPeriod { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; } // Predefined
+        public double Expenses { get; set; }
+        public double Bonuses { get; set; } // Optional
+        public double Deductions { get; set; } // Optional
+        public double Amount { get; set; } // Calculated
+        public ClaimStatus Status { get; set; } // Pending, Approved, or Rejected
+        public DateTime DateSubmitted { get; set; }
+    }
+
+    public enum ClaimStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
+
