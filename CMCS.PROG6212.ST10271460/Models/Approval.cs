eb78a12ec14b3edb0fd73623da4c7174441d6dbc
Reuven-Jon Clements Kadalie <ST10271460@vcconnect.edu.za>
2024-09-09 13:22:15 +0200

    Created a class to handle all approvals of claims (rejected, approved, pending etc)

---
+++ CMCS.PROG6212.ST10271460/Models/Approval.cs
@@ -0,0 +1,13 @@
+﻿namespace CMCS.PROG6212.ST10271460.Models
+{
+    public class Approval
+    {
+        public int ApprovalID { get; set; } // Primary Key
+        public int ClaimID { get; set; } // Foreign Key to Claim
+
+        public int CoordinatorID { get; set; } // Foreign Key to User
+        public int ManagerID { get; set; } // Foreign Key to User
+        public ClaimStatus CoordinatorApprovalStatus { get; set; }
+        public ClaimStatus ManagerApprovalStatus { get; set; }
+        public DateTime FinalApprovalDate { get; set; } // Set when all approvals are done
+    }
